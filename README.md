92777637c6e338e061573fb6e4e9b442bb0814ea
Ranwiesiel <ronggofc@gmail.com>
2025-02-26 19:04:24 +0700

    menambahkan homepage dan memperdetil dokumentasi

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Simple FastAPI (SQLite DB)
 
-Simple REST API with GET, POST, and DELETE operations, using FastAPI python 3.8+, and using SQLite for storage
+Simple REST API with GET, POST, and DELETE operations, using FastAPI python 3.8+, and using SQLite for storage. Link Deploy https://simpleapi.ranwiesiel.serv00.net/
 
 ## Install
 
--- app/main.py
+++ app/main.py
@@ -1,17 +1,28 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from routes.users import user_router
-    
+
+from fastapi.templating import Jinja2Templates
+
+templates = Jinja2Templates(directory="templates")
 
 # init fastapi
 app = FastAPI(
     title="Simple FastAPI",
     description="Simple REST API with GET, POST, and DELETE operations documentations",
     version="0.2",
+    contact={
+        "name": "Github Link",
+        "url": "https://github.com/ranwiesiel/simple-fastapi",
+    },
     docs_url="/api/docs",
 )
 
-app.include_router(user_router, prefix="/api/users", tags=["users"])
+@app.get("/", tags=["Homepage"])
+def homepage(request: Request):
+    return templates.TemplateResponse(name="index.html", request=request)
+
+app.include_router(user_router, prefix="/api/users", tags=["Users"])
 
 
 if __name__ == '__main__':
